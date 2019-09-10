7a759010f1076a4731afc5c900d46a02671c26d5
number04 <schn.matthew@gmail.com>
2019-09-10 23:09:43 +1200

    test

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -51,12 +51,12 @@ class AuthController extends Controller
         //     ], 422);
         // }
 
-        // return (new UserResource($request->user()))
-        //     ->additional([
-        //         'meta' => [
-        //             'token' => $token
-        //         ]
-        //     ]);
+        return (new UserResource($request->user()))
+            ->additional([
+                'meta' => [
+                    'token' => $token
+                ]
+            ]);
     }
 
     public function logout()
@@ -64,8 +64,8 @@ class AuthController extends Controller
         auth()->logout();
     }
 
-    // public function me(Request $request)
-    // {
-    //     return new UserResource($request->user());
-    // }
+    public function me(Request $request)
+    {
+        return new UserResource($request->user());
+    }
 }
