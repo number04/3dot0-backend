f2c4dc6269f0cf483dd41f53fae055e102f57f3b
number04 <schn.matthew@gmail.com>
2019-09-12 22:26:22 +1200

    commands

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,15 +25,9 @@ class Kernel extends ConsoleKernel
      * @return void
      */
 
-    protected function scheduleTimezone()
-    {
-        return 'America/Vancouver';
-    }
-
-
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('theleague:date')->dailyAt('03:25');
+        $schedule->command('theleague:date')->dailyAt('10:28');
         // $schedule->command('theleague:claim')->dailyAt('08:00');
         //
         // $schedule->command('theleague:lineup')->everyMinute();
