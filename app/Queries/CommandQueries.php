fcca4a9f1b5ff60de395303a76ba14bb5118e860
number04 <schn.matthew@gmail.com>
2019-09-12 22:43:30 +1200

    commands

--- app/Queries/CommandQueries.php
+++ app/Queries/CommandQueries.php
@@ -17,12 +17,6 @@ class CommandQueries
 {
     public function lineup()
     {
-        // time(
-        //     CONVERT_TZ(
-        //         NOW(), @@session.time_zone, '+1:00'
-        //     )
-        // )
-
         Lineup::join('player', 'lineup.player_id', '=', 'player.id')
             ->join('schedule', function ($join) {
                 $join->on('lineup.date_id', '=', 'schedule.date_id')->on('player.nhl', '=', 'schedule.team');
@@ -33,7 +27,11 @@ class CommandQueries
             ->update([
                 'lineup.active' => DB::raw(
                     "CASE WHEN schedule.time < (
-                        SELECT NOW()
+                        SELECT time(
+                            CONVERT_TZ(
+                                NOW(), @@session.time_zone, '+1:00'
+                            )
+                        )
                     ) THEN 0 ELSE 1 END"
                 )
             ]);
