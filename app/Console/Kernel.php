d0444435725ae938740e028f1b1ba42e682485bf
number04 <schn.matthew@gmail.com>
2019-09-12 20:48:54 +1200

    commands

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,7 +27,7 @@ class Kernel extends ConsoleKernel
 
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('theleague:date')->everyMinute();
+        $schedule->command('theleague:date')->dailyAt('20:52');
     }
 
     /**
