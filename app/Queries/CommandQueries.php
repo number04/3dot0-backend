e4dea758536515be5ba96a92a09ddd02064fb484
number04 <schn.matthew@gmail.com>
2019-09-14 12:58:23 +1200

    commands

--- app/Queries/CommandQueries.php
+++ app/Queries/CommandQueries.php
@@ -25,20 +25,24 @@ class CommandQueries
 
         // Config::where('key', 'date')->increment('value');
 
-        // Lineup::join('_player', 'lineup.player_id', '=', '_player.id')
-        //     ->join('schedule', function ($join) {
-        //         $join->on('lineup.date_id', '=', 'schedule.date_id')->on('_player.nhl', '=', 'schedule.team');
-        //     })
-        //     ->where('lineup.date_id', function ($query) {
-        //         $query->select('value')->from('config')->where('key', 'date');
-        //     })
-        //     ->update([
-        //         'lineup.active' => DB::raw(
-        //             "CASE WHEN schedule.time < (
-        //                 SELECT NOW()
-        //             ) THEN 0 ELSE 1 END"
-        //         )
-        //     ]);
+        Lineup::join('_player', 'lineup.player_id', '=', '_player.id')
+            ->join('schedule', function ($join) {
+                $join->on('lineup.date_id', '=', 'schedule.date_id')->on('_player.nhl', '=', 'schedule.team');
+            })
+            ->where('lineup.date_id', function ($query) {
+                $query->select('value')->from('config')->where('key', 'date');
+            })
+            ->update([
+                'lineup.active' => DB::raw(
+                    "CASE WHEN schedule.time < (
+                        SELECT time(
+                            CONVERT_TZ(
+                                NOW(), @@session.time_zone, '-8:00'
+                            )
+                        )
+                    ) THEN 0 ELSE 1 END"
+                )
+            ]);
     }
 
     public function date()
