27d43a946d7c1824f46830762c9381e6684fa09d
number04 <schn.matthew@gmail.com>
2019-09-15 22:50:13 +1200

    commands

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -37,10 +37,10 @@ class Kernel extends ConsoleKernel
         $schedule->command('theleague:adds')->mondays()->at('08:00');
         $schedule->command('theleague:lineup')->everyMinute();
 
-        $schedule->command('theleague:date')->dailyAt('10:45');
-        $schedule->command('theleague:matchup')->dailyAt('10:45');
-        $schedule->command('theleague:claim')->dailyAt('10:45');
-        $schedule->command('theleague:waiver')->dailyAt('10:45');
+        $schedule->command('theleague:date')->dailyAt('8:00');
+        $schedule->command('theleague:matchup')->dailyAt('8:00');
+        $schedule->command('theleague:claim')->dailyAt('8:00');
+        $schedule->command('theleague:waiver')->dailyAt('8:00');
     }
 
     /**
